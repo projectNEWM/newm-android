a26236da68119b61e6154b561cc32e3c2e6e37cd
marty <marty@martyulrich.com>
2024-12-19 12:14:36 +0800

    enable tests

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -1,10 +1,10 @@
-##!/bin/bash
-#
-#set -eo pipefail
-#
-#XCFRAMEWORK_PATH="/Users/martyulrich/Repos/newm-mobile/shared/build/XCFrameworks/debug/shared.xcframework"
-#
-#xcodebuild -project ../../iosApp/iosApp.xcodeproj \
-#            -scheme iosApp \
-#            -destination platform=iOS\ Simulator,OS=17.0,name=iPhone\ 11 \
-#            clean build
+#!/bin/bash
+
+set -eo pipefail
+
+XCFRAMEWORK_PATH="/Users/martyulrich/Repos/newm-mobile/shared/build/XCFrameworks/debug/shared.xcframework"
+
+xcodebuild -project ../../iosApp/iosApp.xcodeproj \
+            -scheme iosApp \
+            -destination platform=iOS\ Simulator,OS=17.0,name=iPhone\ 11 \
+            clean build test
