9ab31b121d4104f49363bf3bd6c886a5e4a9a21b
marty <marty@martyulrich.com>
2024-12-19 12:27:50 +0800

    fix test

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 XCFRAMEWORK_PATH="/Users/martyulrich/Repos/newm-mobile/shared/build/XCFrameworks/debug/shared.xcframework"
 
-xcodebuild -project ../../iosApp/iosApp.xcodeproj \
+xcodebuild -project ./iosApp/iosApp.xcodeproj \
             -scheme iosApp \
             -destination platform=iOS\ Simulator,OS=17.0,name=iPhone\ 13 \
             clean build test
